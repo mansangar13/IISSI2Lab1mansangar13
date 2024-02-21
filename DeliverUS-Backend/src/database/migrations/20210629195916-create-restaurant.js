4609d06cf53d8d76ad8a1ab884d161c86f8a8b3a
mansangar13 <mansangar13@alum.us.es>
2024-02-21 18:52:06 +0100

    Primer cambio de prueba

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,9 +6,56 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
-
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      addres: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      averageServiceMinutes: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      logo: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      heroImage: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM('online', 'offline', 'closed', 'temporarily closed')
+      }
     })
   },
   down: async (queryInterface, Sequelize) => {
